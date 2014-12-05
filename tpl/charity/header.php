2c39d5032b74fd18326a677dcfaab9b7582e09d8
falanster-srv <falanster.by@gmail.com>
2014-12-05 16:17:43 +0200

    fork new theme for charity ustomization

---
+++ tpl/charity/header.php
@@ -0,0 +1,203 @@
+<?php
+/********************************************************************************
+ * The contents of this file are subject to the Common Public Attribution License
+ * Version 1.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.wikiarguments.net/license/. The License is based on the Mozilla
+ * Public License Version 1.1 but Sections 14 and 15 have been added to cover
+ * use of software over a computer network and provide for limited attribution
+ * for the Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is Wikiarguments. The Original Developer is the Initial
+ * Developer and is Wikiarguments GbR. All portions of the code written by
+ * Wikiarguments GbR are Copyright (c) 2012. All Rights Reserved.
+ * Contributor(s):
+ *     Andreas Wierz (andreas.wierz@gmail.com).
+ *
+ * Attribution Information
+ * Attribution Phrase (not exceeding 10 words): Powered by Wikiarguments
+ * Attribution URL: http://www.wikiarguments.net
+ *
+ * This display should be, at a minimum, the Attribution Phrase displayed in the
+ * footer of the page and linked to the Attribution URL. The link to the Attribution
+ * URL must not contain any form of 'nofollow' attribute.
+ *
+ * Display of Attribution Information is required in Larger Works which are
+ * defined in the CPAL as a work which combines Covered Code or portions
+ * thereof with code not governed by the terms of the CPAL.
+ *******************************************************************************/
+
+global $sTemplate, $sUser, $sQuery, $sPage;
+$lang = $sTemplate->getLangBase();
+
+// TODO: move to Page<*>
+$filterString = "";
+$tags         = $sQuery->getCurrentTags();
+foreach($tags as $k => $v)
+{
+    if($v != "")
+    {
+        $filterString .= "-".strtolower($v);
+    }
+}
+if(strlen($filterString))
+{
+    $filterString = substr($filterString, 1)."/";
+    $filterStringTrending = "tags/trending/".$filterString;
+}else
+{
+    $filterStringTrending = "";
+}
+
+$filterStringTop      = "tags/top/".$filterString;
+$filterStringNewest   = "tags/newest/".$filterString;
+
+$trendingActive = false;
+$topActive      = false;
+$newestActive   = false;
+
+if($sTemplate->isCurrentPage('default') || $sTemplate->isCurrentPage("group"))
+{
+    $sort = $sPage->getSort();
+
+    $trendingActive = $sort == SORT_TRENDING;
+    $topActive      = $sort == SORT_TOP;
+    $newestActive   = $sort == SORT_NEWEST;
+}
+if($sPage->group())
+{
+    if($sPage->group()->url())
+    {
+        $filterStringTrending   = "groups/".$sPage->group()->url()."/".$filterStringTrending;
+        $filterStringTop        = "groups/".$sPage->group()->url()."/".$filterStringTop;
+        $filterStringNewest     = "groups/".$sPage->group()->url()."/".$filterStringNewest;
+    }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//<?php echo strtoupper($sTemplate->getString("HTML_HEADER_META_LANG")); ?>"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns = "http://www.w3.org/1999/xhtml" xml:lang = "<?php echo $sTemplate->getString("HTML_HEADER_META_LANG"); ?>" lang = "<?php echo $sTemplate->getString("HTML_HEADER_META_LANG"); ?>">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=1100" />
+  <meta http-equiv="content-language" content="<?php echo $sTemplate->getString("HTML_HEADER_META_LANG"); ?>" />
+  <title>[Charity-DEVELOPMENT]<?php echo $sPage->title(); ?></title>
+
+
+  <link rel = "stylesheet" type = "text/css" href = "<?php echo $sTemplate->getTemplateRoot(); ?>css/jquery-ui-1.7.2.custom.css.php" />
+  <link rel = "stylesheet" type = "text/css" href = "<?php echo $sTemplate->getTemplateRoot(); ?>css/style.css.php" />
+
+
+
+  <script type='text/javascript' src='<?php echo $sTemplate->getTemplateRoot(); ?>js/jquery.min.js'></script>
+  <script type='text/javascript' src='<?php echo $sTemplate->getTemplateRoot(); ?>js/jquery-ui.min.js'></script>
+  <script type='text/javascript' src='<?php echo $sTemplate->getTemplateRoot(); ?>js/jquery.jlabel-1.3.min.js'></script>
+  <script type='text/javascript' src='<?php echo $sTemplate->getTemplateRoot(); ?>js/jquery.fancynotification.js'></script>
+  <script type='text/javascript' src='<?php echo $sTemplate->getTemplateRoot(); ?>js/jquery.json.js'></script>
+  <script type='text/javascript' src='<?php echo $sTemplate->getTemplateRoot(); ?>js/wikiargument.js.php'></script>
+  <script type='text/javascript' src='<?php echo $sTemplate->getTemplateRoot(); ?>js/wikiargument_ui.js.php'></script>
+
+
+
+</head>
+<?php flush(); ?>
+<body>
+
+<script>
+<?php if($sPage->group() && $sPage->group()->url()) { ?>
+var wikiargument = new _Wikiargument('<?php echo $sPage->group()->url(); ?>');
+<?php }else { ?>
+var wikiargument = new _Wikiargument('');
+<?php } ?>
+</script>
+
+  <div id = "wrapper">
+    <div id = "header">
+
+      <div id = "header_blue"></div>
+      <div id = "header_white"></div>
+
+      <div id = "header_content">
+<?php if($sPage->group() && $sPage->group()->url()) { ?>
+        <a href = '<?php echo $sTemplate->getRoot()."groups/".$sPage->group()->url()."/"; ?>'>
+          <div id = "header_logo_custom" style = "background: url('<?php echo $sTemplate->getRoot(); ?>custom/<?php echo $sPage->group()->groupId(); ?>/logo.png') no-repeat;"></div>
+        </a>
+<?php }else { ?>
+        <a href = '<?php echo $sTemplate->getRoot(); ?>'>
+          <div id = "header_logo"></div>
+        </a>
+<?php
+   }
+
+if($sPage->getQuestion() && $sPage->getQuestion()->type() == QUESTION_TYPE_UNLISTED)
+{
+?>
+    <div id = "header_logo_unlisted"></div>
+<?php
+    if($sPage->getQuestion()->hasFlag(QUESTION_FLAG_PART_ALL))
+    {
+?>
+    <div id = "header_logo_unlisted_not_logged_in"></div>
+<?php
+    }
+}
+
+?>
+
+        <div id = "header_navigation">
+          <form action = "#" onsubmit = "wikiargument.submitSearch(); return false;">
+          <div class = "navi_point trend <?php echo $trendingActive ? "current_page" : "";?>">
+            <a href = '<?php echo $sTemplate->getRoot(); ?><?php echo $filterStringTrending; ?>'><?php echo $sTemplate->getString("NAVIGATION_WHATS_HOT"); ?></a>
+          </div>
+
+          <div class = "navi_point top <?php echo $topActive ? "current_page" : "";?>">
+            <a href = '<?php echo $sTemplate->getRoot(); ?><?php echo $filterStringTop; ?>'><?php echo $sTemplate->getString("NAVIGATION_TOP"); ?></a>
+          </div>
+
+          <div class = "navi_point neu <?php echo $newestActive ? "current_page" : "";?>">
+            <a href = '<?php echo $sTemplate->getRoot(); ?><?php echo $filterStringNewest; ?>'><?php echo $sTemplate->getString("NAVIGATION_NEWEST"); ?></a>
+          </div>
+
+          <div class = "navi_point">
+              <input type = "text" value = "<?php echo $sTemplate->getString("NAVIGATION_SEARCH_DEFAULT"); ?>" id = "navi_search" name = "navi_search"
+                     onfocus = "if($('#navi_search').val() == '<?php echo $sTemplate->getString("NAVIGATION_SEARCH_DEFAULT"); ?>') {$('#navi_search').val(''); }">
+          </div>
+          </form>
+        </div>
+      </div>
+
+      <div id = "header_menu_wrapper">
+<?php if($sUser->isLoggedIn()) { ?>
+        <div id = "header_menu" class = "header_menu">
+          <div class = "username">
+            <?php echo htmlspecialchars($sUser->getUserName()); ?>
+            <div class = "up_arrow"></div>
+            <div class = "dn_arrow"></div>
+          </div>
+
+          <div class = "hidden profile_menu">
+            <ul class="user_profile_list">
+                <li><div class = "icon_new_question"></div><a href = '<?php echo $sTemplate->getRoot(); ?>new-question/'><?php echo $sTemplate->getString("HEADER_NAVI_NEW_QUESTION"); ?></a></li>
+                <li><div class = "icon_share_page"></div><a href = '#' onclick = "wikiargument.sharePage('<?php echo $sPage->shortUrl() ? $sPage->shortUrl() : ''; ?>');"><?php echo $sTemplate->getString("HEADER_NAVI_SHARE_PAGE"); ?></a></li>
+                <li><div class = "icon_manage_profile"></div><a href = '<?php echo $sTemplate->getRoot(); ?>manage-profile/'><?php echo $sTemplate->getString("HEADER_NAVI_MANAGE_PROFILE"); ?></a></li>
+                <li><div class = "icon_my_profile"></div><a href = '<?php echo $sTemplate->getRoot(); ?>user/<?php echo $sUser->getUserId(); ?>/'><?php echo $sTemplate->getString("HEADER_NAVI_MY_PROFILE"); ?></a></li>
+                <li><div class = "icon_logout"></div><a href = '<?php echo $sTemplate->getRoot(); ?>logout/'><?php echo $sTemplate->getString("HEADER_NAVI_LOGOUT"); ?></a></li>
+<?php foreach($sUser->adminGroups() as $k => $g) { ?>
+                <li class = "user_profile_list_manage_group"><div class = "icon_new_group"></div><a href = '<?php echo $sTemplate->getRoot(); ?>groups/<?php echo $g->url(); ?>/manage-group/'><?php echo htmlspecialchars($g->title()); ?></a></li>
+<?php } ?>
+            </ul>
+          </div>
+        </div>
+<?php }else{ ?>
+          <div class = "header_signup">
+            <a href = '<?php echo $sTemplate->getRoot(); ?>signup/'><?php echo $sTemplate->getString("HEADER_SIGNUP"); ?></a>
+          </div>
+<?php } ?>
+      </div>
+
+    </div>
---
+++ tpl/charity/newQuestion.php
@@ -0,0 +1,127 @@
+<?php
+/********************************************************************************
+ * The contents of this file are subject to the Common Public Attribution License
+ * Version 1.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.wikiarguments.net/license/. The License is based on the Mozilla
+ * Public License Version 1.1 but Sections 14 and 15 have been added to cover
+ * use of software over a computer network and provide for limited attribution
+ * for the Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is Wikiarguments. The Original Developer is the Initial
+ * Developer and is Wikiarguments GbR. All portions of the code written by
+ * Wikiarguments GbR are Copyright (c) 2012. All Rights Reserved.
+ * Contributor(s):
+ *     Andreas Wierz (andreas.wierz@gmail.com).
+ *
+ * Attribution Information
+ * Attribution Phrase (not exceeding 10 words): Powered by Wikiarguments
+ * Attribution URL: http://www.wikiarguments.net
+ *
+ * This display should be, at a minimum, the Attribution Phrase displayed in the
+ * footer of the page and linked to the Attribution URL. The link to the Attribution
+ * URL must not contain any form of 'nofollow' attribute.
+ *
+ * Display of Attribution Information is required in Larger Works which are
+ * defined in the CPAL as a work which combines Covered Code or portions
+ * thereof with code not governed by the terms of the CPAL.
+ *******************************************************************************/
+
+global $sTemplate, $sUser, $sDB, $sPacket, $sPage;
+
+$page       = "";
+$language   = $sTemplate->getLangBase();
+?>
+<?php include($sTemplate->getTemplateRootAbs()."header.php"); ?>
+
+<div id = "content_wide">
+  <div class = "thin">
+    <form action = "<?php echo $sPage->getFormUrl(); ?>" method = "POST" id = "form_new_question">
+      <div class = "new_question">
+        <div class = "row">
+          <div class = "headline"><?php echo $sTemplate->getString("NEW_QUESTION_HEADLINE"); ?></div>
+        </div>
+        <div class = "row">
+          <div class = "label"><?php echo $sTemplate->getString("NEW_QUESTION_TITLE"); ?></div>
+          <div class = "input">
+            <textarea id = "new_question_title" name = "new_question_title" maxlength="<?php echo MAX_QUESTION_CHR_LENGTH ?>"></textarea>
+            <span class="characters_left"><span id="new_question_title_chars_left"><?php echo MAX_QUESTION_CHR_LENGTH ?></span> <?php echo $sTemplate->getString("CHARS_WRITTEN_LEFT"); ?> </span>
+          </div>
+        </div>
+
+        <div class = "row">
+          <div class = "label"><?php echo $sTemplate->getString("NEW_QUESTION_DETAILS"); ?></div>
+          <div class = "input">
+            <textarea id = "new_question_details" name = "new_question_details"></textarea>
+            <span class="characters_left"><span id="new_question_details_chars_left">0</span> <?php echo $sTemplate->getString("CHARS_WRITTEN"); ?> </span>
+          </div>
+        </div>
+
+        <div class = "row">
+          <div class = "label"><?php echo $sTemplate->getString("NEW_QUESTION_TAGS"); ?></div>
+          <div class = "input">
+            <textarea id = "new_question_tags" name = "new_question_tags" maxlength="<?php echo MAX_TAGS_CHR_LENGTH ?>"></textarea>
+            <span class="characters_left"><?php echo $sTemplate->getString("TAGS_DIVIDE"); ?> <span id="new_question_tags_chars_left"><?echo MAX_TAGS_CHR_LENGTH ?></span> <?php echo $sTemplate->getString("CHARS_WRITTEN_LEFT"); ?></span>
+          </div>
+        </div>
+
+        <div class = "row">
+          <div class = "label2"><?php echo $sTemplate->getString("NEW_QUESTION_TYPE"); ?></div>
+          <div class = "input">
+            <select name = "new_question_type" id = "new_question_type" onchange = "wikiargument.changeQuestionType();">
+              <option value = "<?php echo QUESTION_TYPE_LISTED; ?>" selected><?php echo $sTemplate->getString("QUESTION_TYPE_LISTED"); ?></option>
+              <option value = "<?php echo QUESTION_TYPE_UNLISTED; ?>"><?php echo $sTemplate->getString("QUESTION_TYPE_UNLISTED"); ?></option>
+            </select>
+          </div>
+        </div>
+
+        <div class = "row" id = "row_question_flags" style = "display: none;">
+          <div class = "label2"><?php echo $sTemplate->getString("NEW_QUESTION_FLAGS"); ?></div>
+          <div class = "input">
+            <select name = "new_question_flags" id = "new_question_flags" onchange = "wikiargument.changeQuestionFlags();">
+              <option value = "0" selected><?php echo $sTemplate->getString("PARTICIPATION_ONLY_REGISTERED"); ?></option>
+              <option value = "<?php echo QUESTION_FLAG_PART_ALL; ?>"><?php echo $sTemplate->getString("PARTICIPATION_ALL"); ?></option>
+            </select>
+          </div>
+        </div>
+
+        <div class = "row" id = "row_question_unlisted_manipulation" style = "display: none;">
+          <?php echo $sTemplate->getString("NOTICE_NEW_QUESTION_UNLISTED_MANIPULATION"); ?>
+        </div>
+
+        <div class = "row row_submit">
+<?php
+/*
+            <button class = "button_blue clear_form_button" onclick = "$('.new_question textarea').val(''); return false;"><?php echo "Clear form" ?></button>
+*/
+?>
+            <span class = "button_orange" onclick = "$('#form_new_question').submit(); $(this).attr('disabled','disabled'); return false;"><?php echo $sTemplate->getString("SUBMIT_NEW_QUESTION"); ?></span>
+        </div>
+      </div>
+      <input type = "hidden" name = "new_question" value = "1" />
+    </form>
+
+<?php
+/*
+    <div class="writing_tips">
+        <h3>Tips on writing better questions</h3>
+        <ul class="writing_tips_list">
+            <li>Lorem ipsum dollar sit amet, consectetur adipiscing</li>
+            <li>Pellentesque feugiat dapibus auctor. Curabitur</li>
+            <li>Cras tempor metus quis urna tempor consequat. Cum sociis natoque</li>
+            <li>Sed interdum orci sed quam aliquet dapibus. In nisl eros</li>
+            <li>Praesent lacinia ligula sit amet erat pretium ullamcorper</li>
+        </ul>
+    </div>
+*/
+?>
+
+  </div>
+</div>
+
+<?php include($sTemplate->getTemplateRootAbs()."footer.php"); ?>
