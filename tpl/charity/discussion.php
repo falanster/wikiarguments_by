bcbf4b31cbb2a5447b29c3ea254162a3a43fcffd
Soon Real <soonreal@gmail.com>
2015-03-23 12:15:57 +0300

    discussion issue

--- tpl/charity/discussion.php
+++ tpl/charity/discussion.php
@@ -72,7 +72,7 @@ $language   = $sTemplate->getLangBase();
 <p>
 <ul>
 <li><a href="https://github.com/falanster/charity-docs/blob/master/30.12.2014/Proekt-Zakona-OPD-30-12-2014.docx?raw=true">Версія ад 30.12.2014</a>(абмяркоўваем)</li>
-<li><a href="https://github.com/falanster/charity-docs/blob/master/17.11.2014/%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82-%D0%97%D0%B0%D0%BA%D0%BE%D0%BD%D0%B0-%D0%BE%D0%B1-%D0%9E%D0%9F%D0%94-17-11-2014.doc">Версія ад 17.11.2014</a></li>
+<li><a href="https://github.com/falanster/charity-docs/blob/master/17.11.2014/%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82-%D0%97%D0%B0%D0%BA%D0%BE%D0%BD%D0%B0-%D0%BE%D0%B1-%D0%9E%D0%9F%D0%94-17-11-2014.doc?raw=true">Версія ад 17.11.2014</a></li>
 <li><a href="https://github.com/falanster/charity-docs/blob/master/20.10.2014/Proekt-Zakona-OPD-10-20-%D0%B1%D0%B5%D0%B7-%D0%BA%D0%BE%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%B2.doc?raw=true">Версія ад 20.10.2014</a></li>
 <li><a href="https://github.com/falanster/charity-docs/blob/master/10.10.2014/Proekt-Zakona-OPD-10-10-2014.doc?raw=true">Версія ад 10.10.2014</a></li>
 <li><a href="https://github.com/falanster/charity-docs/blob/master/15.09.2014/%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82-%D0%97%D0%B0%D0%BA%D0%BE%D0%BD%D0%B0-%D0%BE%D0%B1-%D0%9E%D0%9F%D0%94-09-15-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B2%D0%B0%D1%80%D0%B8%D0%B0%D0%BD%D1%82.doc?raw=true">Версія ад 15.09.2014</a></li>
